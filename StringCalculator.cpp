63425976294c1c82685affbc7bb68edd17a26861
priyankakbari123 <akbaripriyank123@gmail.com>
2022-07-16 12:19:04 +0530

    Sum of Numbers which are separated by newline char, 3rd task completed

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -5,16 +5,19 @@ using namespace std;
 //Method for finding numbers from string
 vector<int> extractNumbers(string numbers){
     vector<int> nums;
-
     int i;
     string temp="";
+
     for(i=0;i<numbers.length();i++){
-        if(numbers[i]!=',')temp+=numbers[i];
+    	//here 92 is ascii of '\'
+        if(numbers[i]!=',' && numbers[i]!=92 )temp+=numbers[i];
         else{
+        	if(numbers[i]==92)i++;
             nums.push_back(stoi(temp));
             temp="";
         }
     }
+
     if(i==numbers.length() && temp!="")nums.push_back(stoi(temp));
 
     return nums;
@@ -24,15 +27,20 @@ vector<int> extractNumbers(string numbers){
 //Method for Computing sum
 int Add(string numbers){
     if(numbers.empty())return 0;
+
+    //extractNumbers extract numvers from strings
     vector<int> nums=extractNumbers(numbers);
 
+    //sum of extracted numbers
     return accumulate(nums.begin(),nums.end(),0);
 }
 
 
 int main(){
+	//Taking Inputs
     string numbers;
-    cin>>numbers;
+    getline(cin,numbers);
+
     cout<<"Sum : "<<Add(numbers)<<endl;
     return 0;
 }
